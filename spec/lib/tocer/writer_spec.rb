ac34763aa8c873e423d0de44cd0a8d9ec9aca48b
Brooke Kuhlmann <brooke@alchemists.io>
2015-11-21 10:14:53 -0700

    Added `Writer` spec for comment block without messages.
    - Ensures a comment block can be added to a Markdown document
      and be automatically updated even if comment messages are
      missing or different.

--- spec/lib/tocer/writer_spec.rb
+++ spec/lib/tocer/writer_spec.rb
@@ -3,33 +3,33 @@ require "spec_helper"
 describe Tocer::Writer do
   subject { described_class.new test_path }
 
-  describe "#insert", :temp_dir do
+  describe "#write", :temp_dir do
     let(:test_path) { File.join temp_dir, "test.md" }
+    let :contents do
+      "# Introduction\n" \
+      "\n" \
+      "Once upon a time...\n" \
+      "\n" \
+      "<!-- Tocer[start]: Auto-generated, don't remove. -->\n" \
+      "\n" \
+      "# Table of Contents\n" \
+      "\n"\
+      "- [One](#one)\n" \
+      "- [Two](#two)\n" \
+      "- [Three](#three)\n" \
+      "\n" \
+      "<!-- Tocer[finish]: Auto-generated, don't remove. -->\n" \
+      "\n" \
+      "# One\n" \
+      "# Two\n" \
+      "# Three\n" \
+      "\n" \
+      "The end.\n"
+    end
     before { FileUtils.cp fixture_path, test_path }
 
     context "when table of contents exists and is empty" do
       let(:fixture_path) { File.join Dir.pwd, "spec", "support", "fixtures", "toc-empty.md" }
-      let :contents do
-        "# Introduction\n" \
-        "\n" \
-        "Once upon a time...\n" \
-        "\n" \
-        "<!-- Tocer[start]: Auto-generated, don't remove. -->\n" \
-        "\n" \
-        "# Table of Contents\n" \
-        "\n"\
-        "- [One](#one)\n" \
-        "- [Two](#two)\n" \
-        "- [Three](#three)\n" \
-        "\n" \
-        "<!-- Tocer[finish]: Auto-generated, don't remove. -->\n" \
-        "\n" \
-        "# One\n" \
-        "# Two\n" \
-        "# Three\n" \
-        "\n" \
-        "The end.\n"
-      end
 
       it "replaces existing table of contents with new table of contents" do
         subject.write
@@ -39,27 +39,15 @@ describe Tocer::Writer do
 
     context "when table of contents exists" do
       let(:fixture_path) { File.join Dir.pwd, "spec", "support", "fixtures", "toc-existing.md" }
-      let :contents do
-        "# Introduction\n" \
-        "\n" \
-        "Once upon a time...\n" \
-        "\n" \
-        "<!-- Tocer[start]: Auto-generated, don't remove. -->\n" \
-        "\n" \
-        "# Table of Contents\n" \
-        "\n"\
-        "- [One](#one)\n" \
-        "- [Two](#two)\n" \
-        "- [Three](#three)\n" \
-        "\n" \
-        "<!-- Tocer[finish]: Auto-generated, don't remove. -->\n" \
-        "\n" \
-        "# One\n" \
-        "# Two\n" \
-        "# Three\n" \
-        "\n" \
-        "The end.\n"
+
+      it "replaces existing table of contents with new table of contents" do
+        subject.write
+        expect(File.open(test_path, "r").to_a.join).to eq(contents)
       end
+    end
+
+    context "when table of contents uses comments without auto-generated messages" do
+      let(:fixture_path) { File.join Dir.pwd, "spec", "support", "fixtures", "toc-without_comment_messages.md" }
 
       it "replaces existing table of contents with new table of contents" do
         subject.write
---
+++ spec/support/fixtures/toc-without_comment_messages.md
@@ -0,0 +1,12 @@
+# Introduction
+
+Once upon a time...
+
+<!-- Tocer[start] -->
+<!-- Tocer[finish] -->
+
+# One
+# Two
+# Three
+
+The end.
