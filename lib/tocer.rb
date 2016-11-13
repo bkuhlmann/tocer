8cf54362f9e21a01c9685f2e7d7a86d811ccdcd4
Brooke Kuhlmann <brooke@alchemists.io>
2016-11-13 12:15:14 -0700

    Fixed gem requirements order.
    - Ensures dependent objects are loaded first.

--- lib/tocer.rb
+++ lib/tocer.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
+require "tocer/identity"
 require "tocer/elements/comment_block"
 require "tocer/parsers/header"
 require "tocer/transformers/link"
 require "tocer/transformers/text"
 require "tocer/builder"
-require "tocer/cli"
-require "tocer/identity"
 require "tocer/writer"
+require "tocer/cli"
