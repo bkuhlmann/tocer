8b400b3ecea9793ef193f1ded810881a23df6f8b
Brooke Kuhlmann <brooke@alchemists.io>
2016-12-03 11:58:22 -0700

    Added table of contents runner.
    - Encapsulates the generation or updating of table of contents for
      single or multiple files.
    - This will soon be used by the CLI to handle table of contents for
      single or multiple files.

---
+++ lib/tocer/runner.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "pathname"
+
+module Tocer
+  # Generates/updates Table of Contents for files in given path.
+  class Runner
+    def initialize path = "", configuration: {}, writer: Writer
+      @path = Pathname path
+      @configuration = configuration
+      @writer = writer
+    end
+
+    def files
+      return [] unless path.exist?
+      glob = whitelist.empty? ? %(#{path}/**/*) : %(#{path}/**/*{#{whitelist.join ","}})
+      Pathname.glob(glob).select(&:file?)
+    end
+
+    def run
+      files.each { |file| writer.new(file, label: configuration.fetch(:label)).write }
+    end
+
+    private
+
+    attr_reader :path, :configuration, :writer
+
+    def whitelist
+      Array configuration.fetch(:whitelist)
+    end
+  end
+end
---
+++ spec/lib/tocer/runner_spec.rb
@@ -0,0 +1,110 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Tocer::Runner, :temp_dir do
+  let(:path) { "" }
+  let(:label) { "# Test" }
+  let(:whitelist) { [] }
+  let :configuration do
+    {
+      label: label,
+      whitelist: whitelist
+    }
+  end
+  let(:writer_class) { class_spy Tocer::Writer }
+  let(:writer_instance) { instance_spy Tocer::Writer }
+  let(:markdown_file) { Pathname.new File.join(temp_dir, "test.md") }
+  let(:text_file) { Pathname.new File.join(temp_dir, "test.txt") }
+
+  subject { described_class.new path, configuration: configuration, writer: writer_class }
+
+  before do
+    FileUtils.touch markdown_file
+    FileUtils.touch text_file
+  end
+
+  describe "#files" do
+    context "with defaults" do
+      it "answers empty array" do
+        expect(subject.files).to be_empty
+      end
+    end
+
+    context "with path only" do
+      let(:path) { temp_dir }
+
+      it "answers all files in path" do
+        expect(subject.files).to contain_exactly(markdown_file, text_file)
+      end
+    end
+
+    context "with whitelist only" do
+      let(:whitelist) { ["*.md"] }
+
+      it "answers empty array" do
+        expect(subject.files).to be_empty
+      end
+    end
+
+    context "with path and whitelist string" do
+      let(:path) { temp_dir }
+      let(:whitelist) { ".md" }
+
+      it "answers files with matching extensions only" do
+        expect(subject.files).to contain_exactly(markdown_file)
+      end
+    end
+
+    context "with path and whitelist array" do
+      let(:path) { temp_dir }
+      let(:whitelist) { [".md"] }
+
+      it "answers files with matching extensions only" do
+        expect(subject.files).to contain_exactly(markdown_file)
+      end
+    end
+
+    context "with invalid path" do
+      let(:path) { "bogus" }
+
+      it "answers files with matching extensions only" do
+        expect(subject.files).to be_empty
+      end
+    end
+
+    context "with path and invalid whitelist" do
+      let(:path) { temp_dir }
+      let(:whitelist) { ["bogus", "~#}*^"] }
+
+      it "answers files with matching extensions only" do
+        expect(subject.files).to be_empty
+      end
+    end
+  end
+
+  describe "#run" do
+    context "without files" do
+      it "doesn't update files" do
+        subject.run
+        expect(writer_instance).to_not have_received(:write)
+      end
+    end
+
+    context "with files" do
+      let(:path) { temp_dir }
+      let(:whitelist) { [".md"] }
+
+      before do
+        allow(writer_class).to receive(:new)
+          .with(markdown_file, label: label)
+          .and_return(writer_instance)
+      end
+
+      it "updates files" do
+        subject.run
+        expect(writer_instance).to have_received(:write)
+      end
+    end
+  end
+end
