be1cfbea7d1f43d214d700924d1ff13f5bd49141
Brooke Kuhlmann <brooke@alchemists.io>
2021-04-10 18:16:57 -0600

    Added CLI shell
    Necessary to provide the main interface to the command line.

---
+++ lib/tocer/cli/shell.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Tocer
+  module CLI
+    # The main Command Line Interface (CLI) object.
+    class Shell
+      PROCESSORS = {
+        config: Processors::Config.new,
+        build: Processors::Build.new
+      }.freeze
+
+      def initialize parser: Parsers::Assembler.new, processors: PROCESSORS
+        @parser = parser
+        @processors = processors
+      end
+
+      def call arguments = []
+        parse arguments
+
+        case options
+          in config: action then process_config action
+          in build: path then process_build path
+          in version: then puts version
+          in help: then usage
+          else usage
+        end
+      end
+
+      private
+
+      attr_reader :parser, :processors
+
+      def parse arguments = []
+        parser.call arguments
+      rescue StandardError => error
+        puts error.message
+      end
+
+      def process_config(action) = processors.fetch(:config).call(action)
+
+      def process_build(path) = processors.fetch(:build).call(path, options)
+
+      def options = parser.to_h
+
+      def usage = puts(parser.to_s)
+    end
+  end
+end
---
+++ spec/lib/tocer/cli/shell_spec.rb
@@ -0,0 +1,116 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Tocer::CLI::Shell, :runcom do
+  using Refinements::Pathnames
+
+  subject(:shell) { described_class.new processors: processors }
+
+  include_context "with temporary directory"
+
+  let :processors do
+    {
+      build: Tocer::CLI::Processors::Build.new,
+      config: Tocer::CLI::Processors::Config.new(
+        configuration: runcom_configuration,
+        kernel: kernel
+      )
+    }
+  end
+
+  let(:kernel) { class_spy Kernel }
+  let(:fixture_path) { Bundler.root.join "spec/support/fixtures/missing.md" }
+
+  describe "#call" do
+    it "edits configuration" do
+      shell.call %w[--config edit]
+      expect(kernel).to have_received(:system).with(/\$EDITOR\s.+configuration.yml/)
+    end
+
+    it "views configuration" do
+      shell.call %w[--config view]
+      expect(kernel).to have_received(:system).with(/cat\s.+configuration.yml/)
+    end
+
+    it "builds with defaults" do
+      path = temp_dir.join "README.md"
+      fixture_path.copy path
+      shell.call ["--build", temp_dir.to_s]
+
+      expect(path.read).to eq(<<~CONTENT)
+        <!-- Tocer[start]: Auto-generated, don't remove. -->
+
+        ## Table of Contents
+
+        - [One](#one)
+        - [Two](#two)
+
+        <!-- Tocer[finish]: Auto-generated, don't remove. -->
+
+        # One
+        # Two
+      CONTENT
+    end
+
+    it "builds with custom configuration" do
+      path = temp_dir.join "test.md"
+      fixture_path.copy path
+      shell.call ["--build", temp_dir.to_s, "--label", "## Test", "--includes", "*.md"]
+
+      expect(path.read).to eq(<<~CONTENT)
+        <!-- Tocer[start]: Auto-generated, don't remove. -->
+
+        ## Test
+
+        - [One](#one)
+        - [Two](#two)
+
+        <!-- Tocer[finish]: Auto-generated, don't remove. -->
+
+        # One
+        # Two
+      CONTENT
+    end
+
+    it "builds nested path" do
+      path = temp_dir.join("nested/README.md").make_ancestors
+      fixture_path.copy path
+      shell.call ["--build", temp_dir.to_s, "--includes", "**/*.md"]
+
+      expect(path.read).to eq(<<~CONTENT)
+        <!-- Tocer[start]: Auto-generated, don't remove. -->
+
+        ## Table of Contents
+
+        - [One](#one)
+        - [Two](#two)
+
+        <!-- Tocer[finish]: Auto-generated, don't remove. -->
+
+        # One
+        # Two
+      CONTENT
+    end
+
+    it "prints version" do
+      expectation = proc { shell.call %w[--version] }
+      expect(&expectation).to output(/Tocer\s\d+\.\d+\.\d+/).to_stdout
+    end
+
+    it "prints help (usage)" do
+      expectation = proc { shell.call %w[--help] }
+      expect(&expectation).to output(/Tocer.+USAGE.+BUILD OPTIONS/m).to_stdout
+    end
+
+    it "prints usage when no options are given" do
+      expectation = proc { shell.call }
+      expect(&expectation).to output(/Tocer.+USAGE.+BUILD OPTIONS.+/m).to_stdout
+    end
+
+    it "prints error with invalid option" do
+      expectation = proc { shell.call %w[--bogus] }
+      expect(&expectation).to output(/invalid option.+bogus/).to_stdout
+    end
+  end
+end
