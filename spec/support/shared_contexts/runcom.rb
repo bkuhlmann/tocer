6697b5deeb1186636f4e173a5c33a3f83ff4ce81
Brooke Kuhlmann <brooke@alchemists.io>
2021-04-11 13:42:15 -0600

    Added RSpec Runcom shared example
    Necessary to provide a common Runcom configuration which can be reused
    amongst all CLI objects.

---
+++ spec/support/shared_contexts/runcom.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "refinements/pathnames"
+
+RSpec.shared_context "with Runcom", :runcom do
+  using Refinements::Pathnames
+
+  include_context "with temporary directory"
+
+  let :runcom_configuration do
+    Runcom::Config.new gem_configuration_path,
+                       defaults: gem_defaults,
+                       context: Runcom::Context.new(
+                         environment: {"HOME" => temp_dir},
+                         xdg: XDG::Config
+                       )
+  end
+
+  let(:gem_configuration_path) { temp_dir.join("configuration.yml").touch }
+
+  let :gem_defaults do
+    YAML.load_file Bundler.root.join("lib/tocer/cli/configuration/defaults.yml")
+  end
+end
