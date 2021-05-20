8db4ef710f046718c2bb25558b8cf59ffb0435a3
Brooke Kuhlmann <brooke@alchemists.io>
2021-05-19 20:32:28 -0600

    Added version release notes

--- lib/tocer/identity.rb
+++ lib/tocer/identity.rb
@@ -5,7 +5,7 @@ module Tocer
   module Identity
     NAME = "tocer"
     LABEL = "Tocer"
-    VERSION = "12.0.1"
+    VERSION = "12.0.2"
     VERSION_LABEL = "#{LABEL} #{VERSION}"
     SUMMARY = "A command line interface for generating table of contents for Markdown files."
   end
