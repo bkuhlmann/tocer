c863c5873a96b7c3764020eaa5453e047ce4a9f5
Brooke Kuhlmann <brooke@alchemists.io>
2022-01-23 14:29:22 -0700

    Added CLI insert action application container
    Necessary to leverage the new colorized logger.

--- lib/tocer/cli/actions/insert.rb
+++ lib/tocer/cli/actions/insert.rb
@@ -5,15 +5,20 @@ module Tocer
     module Actions
       # Handles the insert action.
       class Insert
-        def initialize runner: Runner.new
+        def initialize runner: Runner.new, container: Container
           @runner = runner
+          @container = container
         end
 
-        def call(configuration) = runner.call(configuration) { |path| puts "  #{path}" }
+        def call configuration
+          runner.call(configuration) { |path| logger.info { "  #{path}" } }
+        end
 
         private
 
-        attr_reader :runner
+        attr_reader :runner, :container
+
+        def logger = container[__method__]
       end
     end
   end
