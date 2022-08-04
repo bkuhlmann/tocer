8073884c1cb15d18c80202f18e09feb526dd286a
Brooke Kuhlmann <brooke@alchemists.io>
2022-08-03 20:40:20 -0600

    Removed registration of duplicate keys within containers
    This behavior is unnecessary for merging containers and removes the
    possiblity of unnecessary mutation of registered objects within the
    container.

--- lib/tocer/cli/actions/container.rb
+++ lib/tocer/cli/actions/container.rb
@@ -9,8 +9,6 @@ module Tocer
       module Container
         extend Dry::Container::Mixin
 
-        config.registry = ->(container, key, value, _options) { container[key.to_s] = value }
-
         merge Tocer::Container
 
         register(:config) { Config.new }
