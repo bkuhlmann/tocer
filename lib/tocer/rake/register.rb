0a05e622b30e33d465b05f3e3823a6e8a0efb303
Brooke Kuhlmann <brooke@alchemists.io>
2022-12-24 07:37:57 -0700

    Added Rake register
    Necessary to define the Rake tasks for this project.

---
+++ lib/tocer/rake/register.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "rake"
+require "refinements/structs"
+require "tocer"
+
+module Tocer
+  module Rake
+    # Registers Rake tasks for use.
+    class Register
+      include ::Rake::DSL
+      using Refinements::Structs
+
+      def self.call = new.call
+
+      def initialize configuration = Container[:configuration], runner: Runner.new
+        @configuration = configuration
+        @runner = runner
+      end
+
+      def call
+        desc "Insert/Update Table of Contents"
+        task :toc, %i[label includes] do |_task, arguments|
+          runner.call configuration.merge(arguments.to_h)
+        end
+      end
+
+      private
+
+      attr_reader :configuration, :runner
+    end
+  end
+end
---
+++ spec/lib/tocer/rake/register_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "tocer/rake/register"
+
+RSpec.describe Tocer::Rake::Register do
+  subject(:tasks) { described_class.new configuration, runner: }
+
+  include_context "with application dependencies"
+
+  let(:runner) { instance_spy Tocer::Runner }
+
+  before { Rake::Task.clear }
+
+  describe ".call" do
+    it "sets up Rake tasks" do
+      described_class.call
+      expect(Rake::Task.tasks.map(&:name)).to contain_exactly("toc")
+    end
+  end
+
+  describe "#call" do
+    before { tasks.call }
+
+    it "calls runner with default arguments" do
+      Rake::Task["toc"].invoke
+
+      expect(runner).to have_received(:call).with(configuration)
+    end
+
+    it "calls runner with custom arguments" do
+      Rake::Task["toc"].invoke "## TOC", %w[one.md two.md]
+
+      expect(runner).to have_received(:call).with(
+        Tocer::Configuration::Content[
+          label: "## TOC",
+          includes: %w[one.md two.md],
+          root_dir: temp_dir
+        ]
+      )
+    end
+  end
+end
